949275ce8192e65a124cb9fbf8330a1eb0cab255
Miracle Nwabueze <m.nwabueze@verifyme.ng>
2021-12-25 10:06:16 +0100

    implement view handler

---
+++ http-server.go
@@ -0,0 +1,13 @@
+package main
+
+// func handler(writer http.ResponseWriter, request *http.Request) {
+
+// }
+
+// func setupServer() {
+// 	http.HandleFunc("/", handler)
+
+// 	log.Fatal(http.ListenAndServe(":8083", http.HandlerFunc(func(writer http.ResponseWriter, req *http.Request) {
+// 		fmt.Println("We get requests")
+// 	})))
+// }
--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
 type Page struct {
 	Title string
@@ -16,7 +21,46 @@ func (page *Page) save() error {
 }
 func main() {
 	// os.
+	p1 := &Page{Title: "TestPage", Body: []byte("I am come to the place")}
+	p1.save()
 
+	p2, _ := loadPage("TestPage")
+
+	fmt.Println(p2.Body)
+
+	fmt.Println("About Starting server")
+
+	setupServer()
+
+}
+
+func handler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Println("Handler collect them", *request.URL, request.URL.Scheme)
+	fmt.Fprintf(writer, "Hi there, I love %s!", request.URL.Path[1:])
+}
+
+// Let's create a handler, viewHandler that will allow users to view a wiki page. It will handle URLs prefixed with "/view/".
+func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	title := request.URL.Path[len("/view/"):]
+
+	page, err := loadPage(title)
+
+	fmt.Println(string(title), page)
+	fmt.Println("We loaded page")
+	if err != nil {
+		writer.Write([]byte("We cannot find the page you are looking for"))
+		return
+	}
+
+	fmt.Fprintf(writer, "<h1>%s</h1><div>%s</div>", page.Title, page.Body)
+
+}
+
+func setupServer() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/view/", viewHandler)
+
+	log.Fatal(http.ListenAndServe(":8083", nil))
 }
 
 func loadPage(title string) (*Page, error) {
