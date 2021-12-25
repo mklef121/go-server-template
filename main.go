965725766709452c61c0b3999e127e68c436e71e
Miracle Nwabueze <m.nwabueze@verifyme.ng>
2021-12-25 20:24:41 +0100

    view saver

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -45,20 +46,58 @@ func viewHandler(writer http.ResponseWriter, request *http.Request) {
 
 	page, err := loadPage(title)
 
-	fmt.Println(string(title), page)
-	fmt.Println("We loaded page")
+	// fmt.Println(string(title), page)
+	// fmt.Println("We loaded page")
 	if err != nil {
-		writer.Write([]byte("We cannot find the page you are looking for"))
+		// writer.Write([]byte("We cannot find the page you are looking for"))
+		http.Redirect(writer, request, "/edit/"+title, http.StatusFound)
 		return
 	}
+	renderTemplate(writer, "view.html", page)
+	// fmt.Fprintf(writer, "<h1>%s</h1><div>%s</div>", page.Title, page.Body)
+}
 
-	fmt.Fprintf(writer, "<h1>%s</h1><div>%s</div>", page.Title, page.Body)
+func editHandler(writer http.ResponseWriter, request *http.Request) {
+	title := request.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
 
+	// fmt.Fprintf(writer, "<h1>Editing %s</h1>"+
+	// 	"<form action=\"/save/%s\" method=\"POST\">"+
+	// 	"<textarea name=\"body\">%s</textarea><br>"+
+	// 	"<input type=\"submit\" value=\"Save\">"+
+	// 	"</form>",
+	// 	p.Title, p.Title, p.Body)
+
+	renderTemplate(writer, "edit.html", p)
+}
+
+func saveHandler(writer http.ResponseWriter, request *http.Request) {
+	title := request.URL.Path[len("/save/"):]
+
+	body := request.FormValue("body")
+
+	pa := &Page{Body: []byte(body), Title: title}
+
+	pa.save()
+
+	fmt.Println("We just finished saving")
+	http.Redirect(writer, request, "/view/"+title, http.StatusFound)
+}
+
+func renderTemplate(writer http.ResponseWriter, templ string, page *Page) {
+	te, _ := template.ParseFiles(templ)
+
+	te.Execute(writer, page)
 }
 
 func setupServer() {
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 
 	log.Fatal(http.ListenAndServe(":8083", nil))
 }
